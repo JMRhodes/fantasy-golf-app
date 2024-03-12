9c8f1fc24aefa872e9d41e71d7fe97de4dd8924f
Justin Rhodes <justinr@tri.be>
2024-03-11 22:27:15 -0500

    Create initial routes

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,20 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/teams', function () {
+    return Inertia::render('Teams');
+})->middleware(['auth', 'verified'])->name('teams');
+
+Route::get('/players', function () {
+    return Inertia::render('Players');
+})->middleware(['auth', 'verified'])->name('players');
+
+Route::get('/tournaments', function () {
+    return Inertia::render('Tournaments');
+})->middleware(['auth', 'verified'])->name('tournaments');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
