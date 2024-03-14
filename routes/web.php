36493103ac99bddcc2d0cb9ab6678a2764c01419
Justin Rhodes <justinr@tri.be>
2024-03-13 20:57:31 -0500

    Laravel upgrade to 11.x

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,32 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Route::get('/', function () {
+//    return Inertia::render('Welcome', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+//});
 
 Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/teams', function () {
-    return Inertia::render('Teams');
+    return Inertia::render('Teams/Index');
 })->middleware(['auth', 'verified'])->name('teams');
 
 Route::get('/players', function () {
-    return Inertia::render('Players');
+    return Inertia::render('Players/Index');
 })->middleware(['auth', 'verified'])->name('players');
 
 Route::get('/tournaments', function () {
-    return Inertia::render('Tournaments');
+    return Inertia::render('Tournaments/Index');
 })->middleware(['auth', 'verified'])->name('tournaments');
 
 Route::middleware('auth')->group(function () {
